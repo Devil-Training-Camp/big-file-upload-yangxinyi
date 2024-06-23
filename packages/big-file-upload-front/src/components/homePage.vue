650caf0e8202deebef6bbe1feb7bac527795b5d7
yangxinyi <1106570123@qq.com>
2024-06-23 19:25:00 +0800

    集成ts及ant-design

---
+++ packages/big-file-upload-front/src/components/homePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <a-upload
+        v-model:file-list="fileList"
+        name="file"
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        :headers="headers"
+        @change="handleChange"
+    >
+        <a-button>
+            <upload-outlined></upload-outlined>
+            Click to Upload
+        </a-button>
+    </a-upload>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const fileList = ref([]);
+const headers = {
+    authorization: 'authorization-text',
+};
+const handleChange = (info: any) => {
+    console.log(info);
+};
+</script>
+
+<style scoped></style>
